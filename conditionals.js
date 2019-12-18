eddc97e27cc6e6acd1478e7b3b6aa85e3101122a
Noah Dumba <noahdumba1@gmail.com>
2019-12-18 23:12:53 +0000

    Conditionalsfor weather

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const raining = true;
+const cold = false;
+const temp = 13
+
+if (temp < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temp < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+console.log("Now you're ready to go outside!");
