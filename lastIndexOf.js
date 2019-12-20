549e52010be1a8b96bd99584566044f2cd42ec68
Noah Dumba <noahdumba1@gmail.com>
2019-12-20 20:17:45 +0000

    find the last instance of a number in an array

---
+++ lastIndexOf.js
@@ -0,0 +1,14 @@
+const lastIndexOf = function(manta, num){
+  var index = -1;
+  for(let i = 0; i < manta.length; i++){
+    if(manta[i] === num)
+      index = i;
+  }
+  return(index)
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
