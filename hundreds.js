74e67ddda20470a1d4589dbe6eed4efc1be941d5
Noah Dumba <noahdumba1@gmail.com>
2019-12-20 18:35:48 +0000

    How many hundreds

---
+++ hundreds.js
@@ -0,0 +1,11 @@
+const howManyHundreds = function(num){
+  let rem = num % 100;
+  let num2 = num - rem;
+  return(num2 / 100)
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
