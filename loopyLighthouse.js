59f06cfa93d394196188351ce3152e5dedc64d25
Noah Dumba <noahdumba1@gmail.com>
2020-01-10 00:13:49 +0000

    changed loopy a bit

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,4 +1,22 @@
-for(let i = 100; i <= 200; i++){
+const loopyLighthouse = function(range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    let multiple0 = (i % multiples[0] === 0);
+    let multiple1 = (i % multiples[1] === 0);
+    let str = '';
+
+    if (multiple0)
+      str += words[0];
+    if (multiple1)
+      str += words[1];
+
+    if (str !== '')
+      console.log(str);
+    else
+      console.log(i);
+  }
+};
+
+/*for(let i = 100; i <= 200; i++){
   if(i%3 === 0 && i%4 === 0){
     console.log('LoopyLighthouse');
   }
@@ -12,3 +30,6 @@ for(let i = 100; i <= 200; i++){
     console.log(i);
   }
 }
+*/
+
+loopyLighthouse([1,20],[3,5],['foo','bar']);
